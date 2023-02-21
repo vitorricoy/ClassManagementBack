95680218775c62754472791343206c92b2e45f5d
Vitor Rodarte <vitorricoy@gmail.com>
2023-02-21 13:40:05 -0300

    File uploading skeleton

--- controller/data_controller.py
+++ controller/data_controller.py
@@ -1,5 +1,20 @@
+from flask import request
 from flask_restful import Resource
+import pandas as pd
+
+from class_management_back.service.data_service import DataService
+
+data_service = DataService()
 
 class DataResource(Resource):
     def post(self):
-        pass
+        file_log = request.files.get('file_log')
+        file_delivery = request.files.get('file_delivery')
+        file_grade = request.files.get('file_grade')
+        if not file_log or not file_delivery or not file_grade:
+            return 'InvalidFile', 400
+        log_data = pd.read_csv(file_log.read())
+        delivery_data = pd.read_csv(file_delivery.read())
+        grade_data = pd.read_csv(file_grade.read())
+        data_service.process_data(log_data, delivery_data, grade_data)
+        return 'success', 200
---
+++ service/data_service.py
@@ -0,0 +1,6 @@
+from pandas import DataFrame
+
+
+class DataService:
+    def process_data(self, log_data: DataFrame, delivery_data: DataFrame, grade_data: DataFrame):
+        pass
