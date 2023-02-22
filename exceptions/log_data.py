5b7c75f8cb3416c9a4dc9868bfa3ea9f0782a0a5
vitorricoy <vitorricoy@gmail.com>
2023-02-22 11:44:08 -0300

    Done some more data processing

---
+++ exceptions/log_data.py
@@ -0,0 +1,10 @@
+class ErrorCreatingClassException(Exception):
+    pass
+
+
+class ErrorCreatingStudentException(Exception):
+    pass
+
+
+class ErrorCreatingModuleException(Exception):
+    pass
--- schema/data.py
+++ schema/data.py
@@ -2,4 +2,25 @@ from pydantic import BaseModel
 
 
 class DataUploadParams(BaseModel):
+    class_name: str
     ignore_user_names: list[str]
+    ignore_activities: list[str]
+
+
+class Class(BaseModel):
+    code: int
+    name: str
+    user_code: int
+
+
+class Student(BaseModel):
+    code: int
+    name: str
+    email: str
+    class_code: int
+
+
+class Module(BaseModel):
+    code: int
+    name: str
+    class_code: int
