9956fee9fdd3eab3ecaa2c376e82be7c4e505e21
vitorricoy <vitorricoy@gmail.com>
2023-04-28 19:01:09 -0300

    Create db script

---
+++ build_db.sql
@@ -0,0 +1,57 @@
+CREATE TABLE user (
+    code serial PRIMARY KEY,
+    name text NOT NULL, 
+    email text NOT NULL, 
+    password text NOT NULL
+);
+
+CREATE TABLE class (
+    code serial PRIMARY KEY,
+    name text NOT NULL,
+    user_code int REFERENCES user(code) NOT NULL
+);
+
+CREATE TABLE student (
+    code serial PRIMARY KEY,
+    name text NOT NULL,
+    email text NOT NULL,
+    class_code int REFERENCES class(code) NOT NULL
+);
+
+CREATE TABLE module (
+    code serial PRIMARY KEY,
+    name text NOT NULL,
+    class_code int REFERENCES class(code) NOT NULL
+);
+
+CREATE TABLE material (
+    code serial PRIMARY KEY,
+    name text NOT NULL,
+    module_code int REFERENCES module(code) NOT NULL
+);
+
+CREATE TABLE class_view (
+    code serial PRIMARY KEY,
+    student_code int REFERENCES student(code) NOT NULL,
+    hour timestamptz NOT NULL
+);
+
+CREATE TABLE material_view (
+    code serial PRIMARY KEY,
+    student_code int REFERENCES student(code) NOT NULL,
+    material_code int REFERENCES material(code) NOT NULL,
+    hour timestamptz NOT NULL
+);
+
+CREATE TABLE activity_delivery (
+    code serial PRIMARY KEY,
+    student_code int REFERENCES student(code) NOT NULL,
+    material_code int REFERENCES material(code) NOT NULL
+);
+
+CREATE TABLE activity_grade (
+    code serial PRIMARY KEY,
+    student_code int REFERENCES student(code) NOT NULL,,
+    material_code int REFERENCES material(code) NOT NULL,
+    grade double NOT NULL
+);
