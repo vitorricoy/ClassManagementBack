9468df9ce60cbeb2deee1d4ccc35132720e0a8ad
vitorricoy <vitorricoy@gmail.com>
2023-06-19 23:44:05 -0300

    Fix?

--- src/class_management_back/app.py
+++ src/class_management_back/app.py
@@ -66,5 +66,15 @@ def index():
     return "Index Page"
 
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
